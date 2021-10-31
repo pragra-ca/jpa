6c05bf3ef537bc6ba192e8c6011bbf421286f73e
ShubhamHirpara <shubhamhirpara61@gmail.com>
2021-10-31 14:05:23 -0400

    added tests

---
+++ src/main/java/co/pragra/learning/pragraec/service/DummyClass.java
@@ -0,0 +1,7 @@
+package co.pragra.learning.pragraec.service;
+
+public class DummyClass {
+    public String demoMethod(){
+        return "matchme";
+    }
+}
--- src/test/java/co/pragra/learning/pragraec/PragraEcApplicationTests.java
+++ src/test/java/co/pragra/learning/pragraec/PragraEcApplicationTests.java
@@ -1,13 +1,22 @@
 package co.pragra.learning.pragraec;
 
+import co.pragra.learning.pragraec.service.DummyClass;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class PragraEcApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+    @Test
+    public void test1(){
+        DummyClass service = new DummyClass();
+        String s = service.demoMethod();
+        assertEquals("matchme",s);
     }
 
 }
